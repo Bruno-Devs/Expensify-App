e0152c5765bea3a0ae8e5c44fd832c89e6b1c184
bruno ugwu <brunougwu5658@gmail.com>
2023-02-14 14:14:21 +0100

    designed option modal, added count to option, added responsiveness for mobile devices

--- src/component/OptionModal.js
+++ src/component/OptionModal.js
@@ -7,10 +7,11 @@ const OptionModal = (props) => (
     onRequestClose={props.handlecloseModal}
     contentLabel="Selected Option"
     ariaHideApp={false}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handlecloseModal}>Close</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handlecloseModal}>Close</button>
   </Modal>
 );
 
